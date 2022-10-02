ddfb819ef48d2ffeaa23e9c3ab662fbec953f2af
Ishva <67134497+hiishva@users.noreply.github.com>
2022-10-01 21:21:11 -0500

    Add files via upload

--- driver.py
+++ driver.py
@@ -1,100 +1,110 @@
 import sys
 from subprocess import Popen, PIPE
 
-
+#PIPES TO THE LOGGER AND ENCRYPTION FILE
 log = Popen(['python3', 'logger.py'], stdout=PIPE, stdin=PIPE, encoding='utf8')
 #encrypt = Popen(['python3', 'encryption.py'], stdout=PIPE, stdin=PIPE, encoding='utf8')
-filename = sys.argv[1]
-log.stdin.write("create\n")
-log.stdin.write(filename + "\n")
+
+filename = sys.argv[1] #Command line arguements, for the file name
+log.stdin.write("create\n") 
+log.stdin.write(filename + "\n") #sends the file name to the logger file to be opened
+
+#VARIABLES
 cmnd = ""
 pswrd= ""
 isPswrdSet = False
-history =[]
+history =[] #Stores the history of calls
+
 while cmnd != "quit":
+    #Displays the menu to the user
     print("Select one of the following options: ")
     command = input("1-password\n2-encrypt\n3-decrypt\n4-history\n5-quit\n")
 
+    #users selects passkey
     if int(command) == 1:
         cmnd = "passkey"
         log.stdin.write("passkey\n")
         log.stdin.flush()
         selectPswd = input ("Select password from history (1) or create a new one(2): \n")
-        if int(selectPswd) == 1:
+        if int(selectPswd) == 1: #Select a new password
             for i in range(len(history)): 
                 print(str(i + 1) + "-" + history[i]+ "\n")
             pswdChoice = input ("Select the password you'd like to use: ")
             pswd = str(history[int(pswdChoice)-1])
             log.stdin.write(str(pswd) + "\n")
             log.stdin.flush()
-        else:
+        else: #Choose a password from the history
             pswd = input("Insert the password you'd like to use: ")
             history.append(str(pswd))
             isPswrdSet = True
             log.stdin.write(str(pswd) +"\n")
             log.stdin.flush()
+    #user selects encrypt
     elif int(command) == 2:
         cmnd = "encrypt"
-        if not isPswrdSet:
+        if not isPswrdSet: #Check if the password is not set
             cmnd = "error"
             log.stdin.write("error\n")
             err = "error"
             print("ERROR PASSWORD NOT SET")
-            log.stdin.write(str(err) + "\n")
+            log.stdin.write(str(err) + "\n") #Sends error to logger file
             cmnd = ""
             log.stdin.flush()
-            print("cmnd: " + cmnd)
             pass
-        else:
+        else: #password is set 
             log.stdin.write("encrypt\n")
             encrypted = input("Choose from history(1) or type word(2)\n")
-            if int(encrypted) == 2:
+            if int(encrypted) == 2: #Selects a new word to encrypt
                 encrpt = input("Insert the word you'd like to encrypt: ")
-                history.append(encrpt)
-                log.stdin.write(str(encrpt) +"\n")
+                history.append(encrpt) #adds the word to the history
+                log.stdin.write(str(encrpt) +"\n") #sends to the logger file to log
                 log.stdin.flush()
-            else:
+            else: #Selects the word from the history
                 for i in range(len(history)): 
                     print(str(i + 1) + "-" + history[i]+ "\n")
                 encryptChoice = input ("Select the word you'd like to encrypt: ")
                 encrypt = str(history[int(encryptChoice)-1])
                 log.stdin.write(str(encrypt) + "\n")
                 log.stdin.flush()
+    #Selects the decrypt command
     elif int(command) == 3:
         cmnd = "decrypt"
         err = "error"
-        if not isPswrdSet:
+        if not isPswrdSet: #checks if the error is set
             cmnd = "error"
             log.stdin.write(str(err) + "\n")
             print("ERROR PASSWORD IS NOT SET\n")
-            log.stdin.write(str(err) + "\n")
+            log.stdin.write(str(err) + "\n") #sends error to logger
             cmnd = ""
             log.stdin.flush()
             pass
-        else:
+        else: #password is set
             log.stdin.write("decrypt\n")
             decrypted = input("Choose a word to be decrypted from history(1) or type a new word to be decrypted(2)\n")
-            if int(decrypted) == 2:
+            if int(decrypted) == 2: #selects to decrypt a new word
                 decrypt = input("Insert the word you'd like to decrypt: ")
-                history.append(decrypt)
-                log.stdin.write(str(decrypt) + "\n")
+                history.append(decrypt) #adds word to history
+                log.stdin.write(str(decrypt) + "\n") #sends to the logger file to log
                 log.stdin.flush()
-            else:
+            else: #Selects from the history
                 for i in range(len(history)): 
                     print(str(i + 1) + "-" + history[i]+ "\n")
                 decryptChoice = input ("Select the word you'd like to decrypt: ")
                 decrypt = str(history[int(decryptChoice)-1])
-                log.stdin.write(str(decrypt) + "\n")
+                log.stdin.write(str(decrypt) + "\n") #Sends the logger file decrypt command
                 log.stdin.flush()
+    #Shows the history 
     elif int(command) == 4:
         cmnd = "history"
         hist = "history"
-        log.stdin.write(str(hist) + "\n")
+        log.stdin.write(str(hist) + "\n") #sends to the logger file to log
         log.stdin.write("history\n")
+        #PRINTING THE HISTORY
         print("\nHISTORY: ")
         for i in range(len(history)):
             print(str(i+1) + "-" + str(history[i]))
         print("\n")
+    #Quits the program
     elif int(command) == 5:
         cmnd = "quit"
         log.stdin.write("halt\n")
--- logger.py
+++ logger.py
@@ -6,37 +6,37 @@ mode = sys.stdin.readline().rstrip()
 
 date_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 while mode != "halt":
+    #opens the logger file
     if mode == "create":
         memloc = sys.stdin.readline().rstrip()
-        file1 = open(memloc, 'a')
+        file1 = open(memloc, 'a') #opens the file in append mode
         date_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file1.write(date_str + " [START] Logging Started\n")
+        file1.write(date_str + " [START] Logging Started\n") #adds the start logging message to file
         sys.stdout.flush()
+    #Adds the password command to the log file
     elif mode == "passkey":
         memloc = sys.stdin.readline().rstrip()
         file1.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " [PASSKEY] Passkey set as " + memloc + "\n")
         sys.stdout.flush()
+    #Adds the error command to the log file
     elif mode == "error":
         memloc = sys.stdin.readline().rstrip()
         file1.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " [ERROR] Passkey not set \n")
         sys.stdout.flush()
+    #Adds the encrypt command to the log file
     elif mode == "encrypt":
         memloc = sys.stdin.readline().rstrip()
         file1.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " [ENCRYPT] Encrypted " + memloc + "\n")
         sys.stdout.flush()
+    #Add the decrypt command to the log file
     elif mode == "decrypt":
         memloc = sys.stdin.readline().rstrip()
         file1.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " [DECRYPT] Decrypted " + memloc + "\n")
         sys.stdout.flush()
+    #Adds the history command to the log file
     elif mode == "history":
         memloc = sys.stdin.readline().rstrip()
         file1.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " [HISTORY] History shown \n")
         sys.stdout.flush()
-    elif mode == "read":
-        file1.write(str(memloc) + "\n")
-        print(memloc)
-        sys.stdout.flush()
-    elif mode == "write":
-        memloc = int(sys.stdin.readline().rstrip())
     mode = sys.stdin.readline().rstrip()
-file1.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+ " [STOP] Logging Stopped\n")
+file1.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+ " [STOP] Logging Stopped\n") #writes stop logging message to the file
